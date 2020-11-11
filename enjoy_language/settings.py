98bc48a308e82ab3d8487528869c704dabf5b547
Hideo <hideo3333@gmail.com>
2020-11-11 22:56:48 +0900

    デプロイ前 修正１

--- enjoy_language/settings.py
+++ enjoy_language/settings.py
@@ -229,4 +229,5 @@ if ENVIRONMENT == 'production':
 
 # Heroku
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500) DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
