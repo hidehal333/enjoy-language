51d00c0c07901245ca7d58b6280a213a8829f574
Hideo <hideo3333@gmail.com>
2020-11-21 16:06:41 +0900

    日記削除（一覧ページ、非同期通信2）修正

--- static/js/script_delete.js
+++ static/js/script_delete.js
@@ -3,17 +3,17 @@ const deleteForm = document.getElementById('delete-form');//モーダル内
 const deleteModalButtons = document.getElementById('diarys');//日記一覧内
 
 
-
 deleteModalButtons.addEventListener("click", (event) => {
     const modal = document.getElementById('modal_delete');
     const diary_card = event.target.closest('.card');
 
-
   if (event.target.classList.contains("delete-modal-button") === true ) {
     const id = event.target.dataset.diary_id;
+    console.log(id);
 
     deleteForm.addEventListener("click", (event) => {
       event.preventDefault();
+      console.log(event);
 
     $.ajax({
         url: ajax_post_delete_url,
@@ -28,7 +28,6 @@ deleteModalButtons.addEventListener("click", (event) => {
         diary_card.remove();
         //modalを非表示
         modal.classList.remove('show');
-
           });
         });
     };
