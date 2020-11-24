50c4389cb56732bc7ce2cc6d60a93b99e56dce6b
Hideo <hideo3333@gmail.com>
2020-11-24 13:48:44 +0900

    テスト2（about, お問い合わせページ、サインアップ）

--- pages/tests.py
+++ pages/tests.py
@@ -2,7 +2,7 @@ from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from . import views
 
-
+#home(トップページ)
 class HomepageTests(SimpleTestCase):
 
 
@@ -36,4 +36,70 @@ class HomepageTests(SimpleTestCase):
         self.assertEqual(
             view.func.__name__,
             views.HomePageView.as_view().__name__
+        )
+
+#Aboutページ
+class AboutPageTests(SimpleTestCase):
+
+
+    def setUp(self):
+        url = reverse('pages:about')
+        self.response = self.client.get(url)
+
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'about.html')
+
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About | Enjoy Language')
+
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be on the page.')
+
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            views.AboutPageView.as_view().__name__
+        )
+
+#お問い合わせページ
+class InquiryPageTests(SimpleTestCase):
+
+
+    def setUp(self):
+        url = reverse('pages:inquiry')
+        self.response = self.client.get(url)
+
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'inquiry.html')
+
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'お問い合わせ')
+
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be on the page.')
+
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/inquiry/')
+        self.assertEqual(
+            view.func.__name__,
+            views.InquiryPageView.as_view().__name__
         )
